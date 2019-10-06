65a4ba9d0d260546dcecfd411d298df89b94000c
Demo User <debian@beaglebone.localdomain>
2019-10-06 12:01:30 -0400

    added driveForward, turn, driveToPoint functions

--- projects/exercises/01/drive_in_line.cpp
+++ projects/exercises/01/drive_in_line.cpp
@@ -1,12 +1,34 @@
 #include <STSL/RJRobot.h>
+#include <cmath>
+
+void driveForward(float x, RJRobot& robot) {
+    robot.setDriveMotors(0.5, -0.5);
+
+    robot.wait(std::chrono::milliseconds(x*800)); 
+    robot.stopMotors(); //Stop the motors
+}
+
+void turn(float angle, RJRobot& robot) {
+    robot.setDriveMotors(0.4, 0.4);
+    robot.wait(std::chrono::milliseconds(angle*4));
+    robot.stopMotors();
+}
+
+void driveToPoint(int x, int y, RJRobot& robot) {
+    float angle = atan2(x, y) * 180 / M_PI;
+    float distance = std::sqrt(x*x + y*y);
+    turn(angle, robot);
+    driveForward(distance, robot);
+    
+}
 
 int main()
 {
     RJRobot robot; //Initialize a RJ Robot
+    //driveForward(2, robot);
+    //turn(180, robot);
     
-    robot.setDriveMotors(1.0, 1.0); //Drive both robot motors forward at max speed
-    robot.wait(2000ms); //Wait for 2000 milliseconds (2 seconds)
-    robot.stopMotors(); //Stop the motors
+    driveToPoint(1, 2, robot);
 
     return 0;
 }
